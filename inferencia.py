66599e26666c74dfd95e0aefa6ba8dee5bb0841a
Giovanni Lucas <giovannilucasmoura@gmail.com>
2023-06-10 17:28:08 -0300

    Separando visualizacao de resposta e camada do objeto, redimensionando resposta do modelo pro tamanho da imagem/anotacao original

--- inferencia.py
+++ inferencia.py
@@ -4,13 +4,17 @@ import os
 import utils
 import numpy as np
 import PIL
+# ----------------
+# Visualização de inferência e avaliação de modelo treinado
+# ----------------
 
+# Caminho da pasta onde o modelo está salvo, no formato SavedModel
 caminho_modelo = 'modelos/modelo_50_(256, 192)_2_5e-06'
 caminho_dados = 'datasets/dataset_avaliacao_voc/'
+mostrar_camada_resposta = False
 
+# Carregando modelo e tamanho do formato da camada de entrada
 modelo = keras.models.load_model(caminho_modelo)
-
-# Formato da camada de entrada
 camada_entrada = modelo.layers[0]
 tamanho_entrada = (camada_entrada.output_shape[0][2], camada_entrada.output_shape[0][1])
 
@@ -20,10 +24,18 @@ if(os.path.isfile(caminho_dados)):
     imagem_predicao = utils.processar_imagem(caminho_dados, tamanho_entrada)
     imagem_predicao = np.expand_dims(imagem_predicao, 0)
     predicao = modelo.predict(imagem_predicao)
-    predicao = tf.argmax(predicao, axis=-1)
-    predicao = predicao[..., tf.newaxis]
+    
+    if mostrar_camada_resposta:
+        predicao = tf.argmax(predicao, axis=-1)
+        predicao = predicao[..., tf.newaxis]
+        predicao = tf.keras.preprocessing.image.array_to_img(predicao[0])
+    else:
+        predicao = tf.keras.preprocessing.image.array_to_img(np.expand_dims(predicao[0][:,:,1], axis=-1))
+    
+    predicao = utils.redimensionar_anotacao(predicao, imagem.size)
+    utils.visualizar(imagem, [], predicao)
 
-    utils.visualizar(imagem, [], predicao[0])
+    
 
 # Se os dados forem uma pasta, assumir que é o formato VOC e também buscar/mostrar as anotações
 if(os.path.isdir(caminho_dados)):
@@ -48,8 +60,13 @@ if(os.path.isdir(caminho_dados)):
 
     for i in range(len(imagens)):
         predicao = predicoes[i]
-        predicao = tf.argmax(predicao, axis=-1)
-        predicao = predicao[..., tf.newaxis]
-        utils.visualizar(imagens[i], anotacoes[i], predicao)
 
-        # utils.visualizar(imagens[i], anotacoes[i], predicao[:,:,1])
+        if mostrar_camada_resposta:
+            predicao = tf.argmax(predicao, axis=-1)
+            predicao = predicao[..., tf.newaxis]
+            predicao = tf.keras.preprocessing.image.array_to_img(predicao)
+        else:
+            predicao = tf.keras.preprocessing.image.array_to_img(np.expand_dims(predicao[:,:,1], axis=-1))
+
+        predicao = utils.redimensionar_anotacao(predicao, tf.keras.preprocessing.image.array_to_img(imagens[i]).size)
+        utils.visualizar(imagens[i], anotacoes[i], predicao)
